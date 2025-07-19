6cb321e35dc81a7d6976eb2a48d93007eac0224f
littleattok <difj1324@gmail.com>
2025-07-20 05:40:19 +0900

    도메인

---
+++ src/main/java/com/example/Post/domain/Posts.java
@@ -0,0 +1,52 @@
+package com.example.Post.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@Table(name="posts")
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable= false, length =32)
+    private String author;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+
+    @Column(nullable = false, length = 60, name="password_hashed")
+    private String password;
+
+    @Column(nullable=false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable=false)
+    private LocalDateTime updatedAt;
+
+
+    @Builder
+    public Posts(String author, String content, String password){
+        this.author = author;
+        this.content = content;
+        this.password = password;
+    }
+
+    public void update(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+}
