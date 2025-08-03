209402c4cb3c2fa579ed6956c863e743791549be
littleattok <difj1324@gmail.com>
2025-08-03 15:07:57 +0900

    사용자 domain 추가

---
+++ src/main/java/com/example/Post/domain/User.java
@@ -0,0 +1,46 @@
+package com.example.Post.domain;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = AccessLevel.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String username;
+
+    @Column (nullabel = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private String profileImageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public User(String username, String password, String nickname, String profileImageUrl) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    
+}
