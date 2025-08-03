52c1bfd6ac24eb5a31f67bc21f744352e7c6c9e8
littleattok <difj1324@gmail.com>
2025-08-03 15:50:36 +0900

    글쓰기 부분 수정

--- src/main/java/com/example/Post/Service/PostService.java
+++ src/main/java/com/example/Post/Service/PostService.java
@@ -8,6 +8,7 @@ import com.example.Post.dto.DeletePostRequest;
 import com.example.Post.dto.PostResponse;
 import com.example.Post.dto.UpdatePostRequest;
 import com.example.Post.repository.PostRepository;
+import com.example.Post.repository.UserRepository;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,56 +22,37 @@ import java.util.stream.Collectors;
 public class PostService {
     private final PostRepository postRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    
 
-    public PostService(PostRepository postRepository, PasswordEncoder passwordEncoder){
-        this.postRepository = postRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+   @Transactional
+   public Long createdAt(PostCreatedRequestDto requestDto, Stirng ipAddress, String username){
+        User user = null;
+        if(username!=null){
+            user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다.");)
+        }
 
-    @Transactional
-    public Long createPost(CreatePostRequest request){
-        String hashedPassword = passwordEncoder.encode(request.getPassword());
         Posts post = Posts.builder()
-                .author(request.getAuthor())
-                .content(request.getContent())
-                .password(hashedPassword)
-                .build();
+            .content(reqeustDto.getContent())
+            .user(user)
+            .authorNickname(user == null ? requestDto.getAuthorNickname() : null )
+            .password(user == null ? passwordEncoder.encode(requestDto.getPassword()) : null)
+            .ipAddress(user == null ? ipAddress : null)
+            .build();
+
         Posts savedPost = postRepository.save(post);
         return savedPost.getId();
-    }
+   }
 
-    public List<PostResponse> getAllPosts(){
-        return postRepository.findAll().stream()
-                .map(PostResponse::from).collect(Collectors.toList());
-    }
+   public List<PostResponseDto> getAllPosts(){
 
-    @Transactional
-    public void updatePost(Long postId, UpdatePostRequest request){
-        Posts post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. Id:" + postId));
+    List<Posts> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC,"createdAt"));
 
-        //1. 요청으로 받은 평문 비밀번호와 DB에 저장된 해시 비밀번호를 비교
-        if (!passwordEncoder.matches(request.getPassword(), post.getPassword())) {
-
-            throw new SecurityException("비밀번호가 일치하지 않습니다.");
-        }
-
-        //2. 비밀번호가 일치하면 수정
-        post.update(request.getAuthor(), request.getContent());
-    }
-
-    @Transactional
-    public void deletePost(Long postId, DeletePostRequest request){
-        Posts post= postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. Id:" + postId));
-
-        if(!passwordEncoder.matches(request.getPassword(), post.getPassword())) {
-            throw new SecurityException("비밀번호가 일치하지 않습니다.");
-        }
-
-        postRepository.delete(post);
-    }
-
+    return posts.stream()
+        .map(PostResponseDto::new)
+        .collect(Collector.ToList());
+   }
 
 
 
