4c966d867bdffadf5ba1070f96d094780518a428
littleattok <difj1324@gmail.com>
2025-08-03 15:13:34 +0900

    Post 수정

--- src/main/java/com/example/Post/domain/Posts.java
+++ src/main/java/com/example/Post/domain/Posts.java
@@ -20,12 +20,21 @@ public class Posts extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-    @Column(nullable= false, length =32)
-    private String author;
 
     @Lob
     @Column(nullable = false)
     private String content;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")// 외래키 user_id
+    private User user;          // 회원인 경우 이 필드에 저장됨
+
+    private String authorNickname;
+
+    private String password;
+
+    private String ipAddress;
 
 
     @Column(nullable = false, length = 60, name="password_hashed")
@@ -33,14 +42,14 @@ public class Posts extends BaseTimeEntity {
 
 
     @Builder
-    public Posts(String author, String content, String password){
-        this.author = author;
+    public Posts(String content, User user, String authorNickname, String password, String ipAddress){
         this.content = content;
+        this.user = user;
+        this.authorNickname = authorNickname;
         this.password = password;
+        this.ipAddress = ipAddress;
     }
-
     public void update(String author, String content) {
-        this.author = author;
         this.content = content;
     }
 }
