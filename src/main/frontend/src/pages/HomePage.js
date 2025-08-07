3e6c57d4c89305117708463a5a94a30222226d56
littleattok <difj1324@gmail.com>
2025-08-08 02:42:33 +0900

    홈페이지 수정

--- src/main/frontend/src/pages/HomePage.js
+++ src/main/frontend/src/pages/HomePage.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+import {getAllPosts} from '../api/posts';
+
+import {PostForm} from '../features/Posts/PostForm';
+import {PostCard} from '../features/Posts/PostCard';
+
+const HomePageContainer = styled.div`
+    width: 100%;
+    `;
+
 
 function HomePage(){
+
+    const [posts, setPosts]= useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchPosts = useCallback(async () =>{
+        try{
+            setLoading(true);
+            const response = await getAllPosts();
+            setPosts(response.data);
+        } catch (error) {
+            console.error("게시글을 불러오는 데 실패했습니다.", error);
+        } finally {
+            setLoading(false);
+        }},[]);
+
+    useEffect(()=>{
+        fetchPosts();
+    },[fetchPosts]);
+    if(loading){
+        return <div>불러오는 중...</div>;
+    }
+
+    
     return (
-        <div>
+        <HomePageContainer>
             <h1>전체 게시글 피드</h1>
-            {/*여기에 추후 컴포넌트 배치 예정*/}
-        </div>
+                <PostForm onPostCreated={fetchPosts}/>
+                <div>
+                    {posts.map(post =>(
+                        <PostCard key={post.id} post ={post}/>
+                    ))}
+                </div>
+        </HomePageContainer>
     );
 }
 
