b155db65b9292485c717cf82a0e2bf90df55946d
littleattok <difj1324@gmail.com>
2025-08-11 15:38:32 +0900

    JWT 필터 적용

--- src/main/java/com/example/Post/config/SecurityConfig.java
+++ src/main/java/com/example/Post/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.example.Post.config;
 
+import com.example.Post.jwt.jwtUtil;
+import com.example.Post.jwt.JwtAuthenticationFilter;
+import com.example.Post.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +14,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final jwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -19,21 +34,21 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // http.csrf(csrf -> csrf.disable()) 
-        //     .authorizeHttpRequests(authorize -> authorize
-        //             .requestMatchers("/api/**").permitAll()
-        //             .anyRequest().authenticated()
-        //     );
-        // return http.build();
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter (jwtUtil, userDetailsService);
+    }
 
-        http.csrf(AbstractHttpConfigurer::disable)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/users/**").permitAll()
-                .requestMatchers("api/**").authenticated()
-                .anyRequest().permitAll());
-
+                                        .requestMatchers("/api/users/**").permitAll()
+                                        .requestMatchers(HttpMethod.GET ,"/api/posts/**").permitAll()
+                                        .anyRequest().authenticated()
+                                        )
+            .addFilterBefore(jwtAuthenticationFilter(),UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
